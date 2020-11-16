0298eb5e20104551e6b65e9479e6d7509fd35ad2
nakhodnov17 <nakhodnov17@gmail.com>
2020-11-16 19:57:22 +0300

    Use javascript into simplest server

--- FlaskExample/src/simplest_server.py
+++ FlaskExample/src/simplest_server.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/js_test')
 def get_index():
-    return 'Hello'
     return '<html><center><script>document.write("Hello, i`am js!")</script></center></html>'
 
 
